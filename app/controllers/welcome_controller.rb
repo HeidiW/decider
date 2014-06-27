d570f6490e2b2fa4516bf0f9fcd9b9f05ef449b8
Aldric Giacomoni <trevoke@gmail.com>
2014-06-27 17:01:46 -0400

    reindenting

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
 
-	before_action:current_user
+  before_action:current_user
 
-    def index
-    	@shows = @current_user.shows
-	end
-	
+  def index
+    @shows = @current_user.shows
+  end
+
 end
